6cb39fb161e732431d1cfca5ef54d9eff3d5354e
Noga Vigdor <noga.vigdor@gmail.com>
2024-02-28 22:40:31 +0100

    created index view which displays some demi tasks

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,67 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+class Task
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $description,
+        public ?string $long_description,
+        public bool $completed,
+        public string $created_at,
+        public string $updated_at
+    ) {
+    }
+}
 
-Route::get('/', function () {
-    return view('welcome');
-});
+$tasks = [
+    new Task(
+        1,
+        'Do some shopping',
+        'Task 1 description',
+        'Task 1 long description',
+        false,
+        '2023-03-01 12:00:00',
+        '2023-03-01 12:00:00'
+    ),
+    new Task(
+        2,
+        'Submit compulsary assignment',
+        'Task 2 description',
+        null,
+        false,
+        '2023-03-02 12:00:00',
+        '2023-03-02 12:00:00'
+    ),
+    new Task(
+        3,
+        'Learn Vue.js',
+        'Task 3 description',
+        'Task 3 long description',
+        true,
+        '2024-02-03 12:00:00',
+        '2023-03-03 12:00:00'
+    ),
+    new Task(
+        4,
+        'Do laundary',
+        'Task 4 description',
+        null,
+        false,
+        '2021-02-04 12:00:00',
+        '2025-03-04 12:00:00'
+    ),
+];
+
+Route::get('/', function () use ($tasks) {
+    return view('index',
+        [
+            'tasks' => $tasks
+        ]
+    );
+})->name('tasks.index');
+
+Route::get('/{id}', function ($id) {
+    return 'one single task';
+})->name('tasks.show');
